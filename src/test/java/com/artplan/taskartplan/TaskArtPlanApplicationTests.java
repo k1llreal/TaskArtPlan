5d5761cf4586688278c8d7156248a76b60cd95b6
k1llreal <k1llreally@yandex.ru>
2022-07-01 17:43:43 +0300

    Initial commit

---
+++ src/test/java/com/artplan/taskartplan/TaskArtPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artplan.taskartplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskArtPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
